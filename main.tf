949a46d64fbf258806580ff47a4da7d9463582ec
MD AMIRUL HAQUE <haqueamir70@gmail.com>
2026-09-16 16:12:41 +0300

    feat: add Azure virtual network and subnets

--- main.tf
+++ main.tf
@@ -2,3 +2,24 @@ resource "azurerm_resource_group" "landing_zone" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "landing_zone" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.landing_zone.name
+  address_space       = var.vnet_address_space
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = var.app_subnet_name
+  resource_group_name  = azurerm_resource_group.landing_zone.name
+  virtual_network_name = azurerm_virtual_network.landing_zone.name
+  address_prefixes     = var.app_subnet_address_prefixes
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = var.private_subnet_name
+  resource_group_name  = azurerm_resource_group.landing_zone.name
+  virtual_network_name = azurerm_virtual_network.landing_zone.name
+  address_prefixes     = var.private_subnet_address_prefixes
+}
--- variables.tf
+++ variables.tf
@@ -9,3 +9,38 @@ variable "location" {
   type        = string
   default     = "uaenorth"
 }
+variable "vnet_name" {
+  description = "Name of the Azure Virtual Network"
+  type        = string
+  default     = "vnet-landingzone-dev"
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the Azure Virtual Network"
+  type        = list(string)
+  default     = ["10.10.0.0/16"]
+}
+
+variable "app_subnet_name" {
+  description = "Name of the application subnet"
+  type        = string
+  default     = "snet-app"
+}
+
+variable "app_subnet_address_prefixes" {
+  description = "Address prefixes for the application subnet"
+  type        = list(string)
+  default     = ["10.10.1.0/24"]
+}
+
+variable "private_subnet_name" {
+  description = "Name of the private subnet"
+  type        = string
+  default     = "snet-private"
+}
+
+variable "private_subnet_address_prefixes" {
+  description = "Address prefixes for the private subnet"
+  type        = list(string)
+  default     = ["10.10.2.0/24"]
+}
